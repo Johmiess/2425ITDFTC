c86d09e65aebd9b9e0f677cfb34339520c1dd3cc
Johmm <76572867+Lightwastaken@users.noreply.github.com>
2025-01-25 04:30:30 -0800

    redpark

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/redpark.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous(name = "blue_obs", group = "Autonomous")
+public class redpark extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(-24, 70, Math.toRadians(-90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder p1 = drive.actionBuilder(initialPose)
+                .strafeTo(new Vector2d(-60,60));
+
+        telemetry.update();
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Action a1 = p1.build();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                    a1
+                )
+        );
+    }
+}
