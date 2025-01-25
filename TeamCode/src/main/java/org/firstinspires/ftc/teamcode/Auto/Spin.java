924a4308f0a0508d39eb22b11f737151520006ed
phantom <koo011707@gmail.com>
2025-01-25 05:50:15 -0800

    "just turns" - john

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Spin.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous(name = "spin", group = "Autonomous")
+public class Spin extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(24, -60, Math.toRadians(-90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder p1 = drive.actionBuilder(initialPose)
+                        .turn(Math.toRadians(90));
+
+        telemetry.update();
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Action a1 = p1.build();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        a1
+                )
+        );
+    }
+}
